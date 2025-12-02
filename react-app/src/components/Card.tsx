8b90f8ff14f6a3619dbdde2285908e99b152a8d2
EdithMR <edithmandujano001@gmail.com>
2025-12-02 12:43:42 -0600

    Se inicializa app

---
+++ react-app/src/components/Card.tsx
@@ -0,0 +1,20 @@
+
+
+function Card(){
+  return <div className="card" style={{width:"350px"}}>
+  <div className="card-body" >
+    <CardBody/>
+  </div>
+</div>
+}
+
+export function CardBody(){
+  return 
+  <>
+    <h5 className="card-title">Card title</h5>
+    <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" className="btn btn-primary">Card link</a>
+  </>
+}
+export default  Card;
